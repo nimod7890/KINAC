970ae50bfe6d4a97856ff5013a1532614bbfa008
nimod7890 <nimod1234@g.skku.edu>
2023-01-04 19:09:10 +0900

    fe: add result modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,30 @@
 import Layout from "../components/Layout";
-import { Button, Flex, Stack } from "@chakra-ui/react";
-import AutoCompleteInputComponent from "../components/AutoCompleteInput";
-import { useState } from "react";
-import DeviceRadioGroup from "../components/DeviceRadioGroup";
-import { useRecoilState, useRecoilValue } from "recoil";
 import {
-  assetState,
-  characterState,
-  deviceState,
-  onCllickState
-} from "../recoil";
+  Button,
+  Divider,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  Text,
+  useDisclosure
+} from "@chakra-ui/react";
+import AutoCompleteInputComponent from "../components/AutoCompleteInput";
+import DeviceRadioGroup from "../components/DeviceRadioGroup";
 import CharacterRadioGroup from "../components/CharacterRadioGroup";
+import { useRecoilValue } from "recoil";
+import { assetState, characterState, deviceState } from "../recoil";
 
 export default function Home() {
-  // const [show, setShow] = useState(false);
   const asset = useRecoilValue<string>(assetState);
   const device = useRecoilValue<number>(deviceState);
   const character = useRecoilValue<string[]>(characterState);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <Layout>
@@ -33,7 +41,7 @@ export default function Home() {
                     w={200}
                     alignSelf="end"
                     colorScheme={"facebook"}
-                    onClick={(e) => {}}
+                    onClick={onOpen}
                     _active={{
                       transform: "scale(0.98)"
                     }}
@@ -45,6 +53,40 @@ export default function Home() {
             )}
           </>
         )}
+        <Modal
+          closeOnOverlayClick={false}
+          onClose={onClose}
+          isOpen={isOpen}
+          size="md"
+          isCentered
+        >
+          <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(3px)" />
+          <ModalContent>
+            <ModalHeader fontWeight={"bold"} color="facebook.800">
+              {asset}
+            </ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Text fontSize={14}>
+                구분: {device == 1 ? "Hardware" : "Software"}
+                <br />
+                특성: {character[0]} - {character[1]}
+              </Text>
+              <Divider mt={2} mb={5} borderColor="#385898" />
+              <Text fontWeight={"bold"} color="facebook.800">
+                Attack Pathway:
+              </Text>
+              <Text fontWeight={"bold"} color="facebook.800">
+                Attack Case:
+              </Text>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme={"facebook"} onClick={onClose}>
+                Close
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
       </Stack>
     </Layout>
   );
