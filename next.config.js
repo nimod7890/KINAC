ec1c07532e39b40348c5a87f19e405f7918d71d6
nimod7890 <nimod1234@g.skku.edu>
2023-01-08 07:45:35 +0900

    update for deploy

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   assetPrefix:
     process.env.NODE_ENV === "production"
-      ? "http://nimod7890.github.io/KINAC"
+      ? "https://nimod7890.github.io/KINAC"
       : "",
   trailingSlash: true
 };
