05529b95179b5384fd3f20607c8b66e8c521b2aa
nimod7890 <nimod1234@g.skku.edu>
2023-01-08 08:13:27 +0900

    update for deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,13 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { RecoilRoot } from "recoil";
-import { PortfolioProvider } from "../context";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PortfolioProvider value={null}>
-      <RecoilRoot>
-        <ChakraProvider>
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </RecoilRoot>
-    </PortfolioProvider>
+    <RecoilRoot>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </RecoilRoot>
   );
 }
