dccfbe28dc7bd37e9ef199399707aabf85fc5b26
nimod7890 <nimod1234@g.skku.edu>
2023-01-08 07:58:13 +0900

    deploy to gh-pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://nimod7890.github.io/KINAC"
-      : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "." : "",
   trailingSlash: true
 };
 
