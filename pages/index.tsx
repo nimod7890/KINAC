0acf8e62682723f1e73443d667bcdde5300dd884
nimod7890 <nimod1234@g.skku.edu>
2023-01-08 03:28:52 +0900

    update for deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,28 +32,20 @@ export default function Home() {
     <Layout>
       <Stack spacing={7}>
         <AutoCompleteInputComponent />
-        {asset && (
-          <>
-            <DeviceRadioGroup />
-            {device !== 0 && (
-              <>
-                <CharacterRadioGroup />
-                {character.length == 2 && (
-                  <Button
-                    w={200}
-                    alignSelf="end"
-                    colorScheme={"facebook"}
-                    onClick={onOpen}
-                    _active={{
-                      transform: "scale(0.98)"
-                    }}
-                  >
-                    Submit
-                  </Button>
-                )}
-              </>
-            )}
-          </>
+        <DeviceRadioGroup />
+        <CharacterRadioGroup />
+        {character.length == 2 && (
+          <Button
+            w={200}
+            alignSelf="end"
+            colorScheme={"facebook"}
+            onClick={onOpen}
+            _active={{
+              transform: "scale(0.98)"
+            }}
+          >
+            Submit
+          </Button>
         )}
         <Modal
           closeOnOverlayClick={false}
