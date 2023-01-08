be8f3efe4c3dab978bd677cc731def7f9df4d8ec
nimod7890 <nimod1234@g.skku.edu>
2023-01-08 09:07:55 +0900

    deploy to gh-pages

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === "production" ? "." : ".",
-  trailingSlash: true
-};
-
-module.exports = nextConfig;
