32e9cff1981d942b7cbc8b8d44644a82de8ce504
nimod7890 <nimod1234@g.skku.edu>
2023-01-08 10:16:25 +0900

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,6 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta
-          httpEquiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-      </Head>
       <body>
         <Main />
         <NextScript />
